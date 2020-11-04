5ed24161ad23d565420ca7cb3f2cc240b8bb4531
lilsherlock <potatogames15@gmail.com>
2020-11-03 21:16:45 -0500

    update

---
+++ models/engine/file_storage.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+
+"""FileStorage Module"""
+import json
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.places import Place
+from models.review import Review
+
+from os import path
+
+
+class FileStorage:
+    """ file storage """
+
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """ all method """
+        return self.__objects
+
+    def new(self, obj):
+        """ new method """
+        key = obj.__class__.__name__ + "." + obj.id
+        self.__objects[key] = obj
+
+    def save(self):
+        """ save method """
+        new_dict = {}
+        for key, value in self.__objects.items():
+            new_dict[key] = value.to_dict()
+        with open(self.__file_path, 'w') as new_file:
+            new_file.write(json.dumps(new_dict))
+
+    def reload(self):
+        """ reload method """
+        if path.exists(self.__file_path):
+            with open(self.__file_path, mode='r', encoding='utf-8') as f:
+                json_dict = json.loads(f.read())
+                for key, value in json_dict.items():
+                    self.__objects[key] = eval(value['__class__'])(**value)
